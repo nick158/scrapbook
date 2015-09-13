2e2e36f4b4f1dd791357a9ac11c398d27f42d151
nick158 <nicholasdlchan60@gmail.com>
2015-09-13 17:35:52 -0400

    that home page tho

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
-var app = angular.module('scrapbook', []);
-var $container = $('#container');
+var app = angular.module('app', ['ngRoute']);
 // init
-$container.packery({
-  itemSelector: '.item',
-  gutter: 0
+app.config(['$routeProvider',function($routeProvider){
+	$routeProvider.when('/home',{
+		templateUrl: 'home.html',
+		controller: 'homeController'
+	}).otherwise({
+		redirectTo: '/home',
+	});
+}]);
+app.controller('homeController', function($scope){
+	$scope.getRandom = function(){
+		return Math.round(Math.random()) ;
+	};
+	$scope.colors = ['blue','red', 'green'];
+	$scope.chapters = ["Scarcity & Decision Making","National Decision Making",'Productive Resources','Demand, Supply & Markets','Spectrum of Markets','Distribution','Labour Unions', 'The Entrpreneur',
+	'Business Organization & Finance', 'Consumption & Savings','Government Expenditures & Revenues', 'Money & Banking','Unemployment', 'Inflation', 'Economic Growth & Productivity', 'International Trade'];
 });
