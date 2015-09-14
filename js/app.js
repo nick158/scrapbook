dca195247695829f92b20cd4fabba9362a184018
nick158 <nicholasdlchan60@gmail.com>
2015-09-13 22:26:36 -0400

    more home page things

--- js/app.js
+++ js/app.js
@@ -9,10 +9,37 @@ app.config(['$routeProvider',function($routeProvider){
 	});
 }]);
 app.controller('homeController', function($scope){
+	 $(function(){
+        $("#subtitle1").typed({
+            strings: ["In Economics you learn about "],
+            typeSpeed: -3,
+            loop: false,
+            showCursor:false,
+            onStringTyped: function() {
+
+
+            		 $(function(){
+        $("#subtitle").typed({
+            strings: ["scarcity.", "people.", 'demand and supply curves :(','wants and needs.','economic growth.', 'money $$$.', 'unemployment.', 'jobs and labour.','productivity.', 'interest rates.'],
+            typeSpeed: -10,
+            loop: true,
+            backDelay: 4000,
+            backSpeed: -3,
+   		 });
+    });
+            }
+    });
+    });
+	$('#scroll').on('click', function(){
+		$('html,body').animate({
+			scrollTop: $('.item:first').offset().top - 40
+		},900);
+
+	});
 	$scope.getRandom = function(){
 		return Math.round(Math.random()) ;
 	};
 	$scope.colors = ['blue','red', 'green'];
-	$scope.chapters = ["Scarcity & Decision Making","National Decision Making",'Productive Resources','Demand, Supply & Markets','Spectrum of Markets','Distribution','Labour Unions', 'The Entrpreneur',
+	$scope.chapters = ["Scarcity & Decision Making","National Decision Making",'Productive Resources','Demand, Supply & Markets','Spectrum of Markets','Distribution','Labour Unions', 'The Entrepreneur',
 	'Business Organization & Finance', 'Consumption & Savings','Government Expenditures & Revenues', 'Money & Banking','Unemployment', 'Inflation', 'Economic Growth & Productivity', 'International Trade'];
 });
